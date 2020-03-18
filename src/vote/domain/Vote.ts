a7d64f182eb84cf316ff53063bd8e563fb846e78
XxshiftxX <contact@dev-shift.me>
2020-03-18 16:28:25 +0900

    Vote 도메인 추가

---
+++ src/vote/domain/Vote.ts
@@ -0,0 +1,26 @@
+export interface Vote {
+  data: VoteData;
+  email: string;
+}
+
+export interface VoteData {
+  problem?: number[];
+  pitch?: string[];
+  voice?: string[];
+  funny?: string[];
+  content?: string[];
+  original?: string[];
+  sleep?: string;
+  unit?: string;
+  new?: string;
+  grow?: string;
+  master?: string[];
+  custom?: {
+    episode: string;
+    content: string;
+  }[];
+  message?: {
+    name: string;
+    content: string;
+  }[];
+}
