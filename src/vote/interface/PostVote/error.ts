1ce20ce6ab5cd3568e5630f1567903dd5be37cb8
XxshiftxX <contact@dev-shift.me>
2020-03-27 12:52:03 +0900

    add log on interface layer

--- src/vote/interface/PostVote/error.ts
+++ src/vote/interface/PostVote/error.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
+import Log from '../../../logger';
 
 const handleError = (error: Error, req: Request, res: Response) => {
+  Log.info('[INTERFACE] postVoteController ErrorHandler started');
   switch (error) {
     default:
+      Log.info(`default case: ${JSON.stringify(error)}`);
       res.status(500).json({ succeed: false, result: error });
   }
 };
--- src/vote/interface/PostVote/index.ts
+++ src/vote/interface/PostVote/index.ts
@@ -1,24 +1,35 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 
+import Log from '../../../logger';
 import handleError from './error';
-
 import Vote from '../../domain/Vote';
 import createVote from '../../application/createVote';
 
 const postVoteController = async (req: Request, res: Response) => {
+  Log.info('[INTERFACE] postVoteController started');
+  Log.info(`body: ${JSON.stringify(req.body)}`);
+
   // VALIDATION
+  Log.debug('[INTERFACE] validation started');
   const errors = validationResult(req);
+  Log.debug(`[INTERFACE] validation: ${JSON.stringify(errors.mapped())}`);
   if (!errors.isEmpty()) {
+    Log.info(`[INTERFACE] validation error occurred: ${JSON.stringify(errors.array())})`);
     return res.status(400).json({ succeed: false, errors: errors.array() });
   }
 
+  Log.debug('[INTERFACE] vote creation started');
   const vote: Vote = req.body;
+  Log.debug(`[INTERFACE] vote created: ${JSON.stringify(vote)}`);
+
   try {
     const action = await createVote(vote);
 
+    Log.info(`[INTERFACE] postVoteController success: ${action}`);
     res.json({ succeed: true, result: action });
   } catch (error) {
+    Log.error('[INTERFACE] postVoteController error: creating vote');
     handleError(error, req, res);
   }
 };
