6a10ab29fe18432b4aa3d410324235e037c75076
XxshiftxX <contact@dev-shift.me>
2020-03-30 11:56:46 +0900

    add log on application layer

--- src/vote/application/createVote.ts
+++ src/vote/application/createVote.ts
@@ -1,7 +1,10 @@
 import Vote from '../domain/Vote';
 import * as DAO from '../infrastructure/VoteDAO';
+import Log from '../../logger';
 
 const createVote = async (vote: Vote) => {
+  Log.info('[APPLICATION] createVote start');
+  Log.info(`vote: ${JSON.stringify(vote)}`);
   const result = await DAO.createVote(vote);
 
   return result.id as string;
